a7d3de255a59a4d60acf93a4845b564c37c7091b
Lionel Tchami <65927599+apotitech@users.noreply.github.com>
2024-03-10 20:48:47 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-# configure aws credentials
+
+# Configure AWS credentials
+
 # Prompt the user for AWS access key ID
-read -p "Enter your AWS access key ID: " aws_access_key_id
+read -rp "Enter your AWS access key ID: " aws_access_key_id
 
 # Prompt the user for AWS secret access key
-read -p "Enter your AWS secret access key: " aws_secret_access_key
+read -rp "Enter your AWS secret access key: " aws_secret_access_key
 
 # Write the AWS credentials to the ~/.aws/credentials file
-cat << EOF > ~/.aws/credentials
+cat <<EOF > ~/.aws/credentials
 [default]
 aws_access_key_id = $aws_access_key_id
 aws_secret_access_key = $aws_secret_access_key
@@ -18,8 +20,9 @@ echo "AWS credentials have been saved to ~/.aws/credentials."
 
 # Initialize Terraform
 terraform init
-# Validate the code
-terraform validate && terraform plan -out=plan.out 
+
+# Validate and plan the Terraform configuration
+terraform validate && terraform plan -out=plan.out
+
 # Apply Terraform configuration
 terraform apply --auto-approve "plan.out"
-
